92fed06517ca1fc801321e9bb2bee22819d90f2f
Karol Celeita <karol.celeitag@gmail.com>
2024-03-21 16:50:52 -0500

    Pagination Funcionality

--- home/views.py
+++ home/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator
+
 
 #List View
 
 def post_list(request):
     posts = Post.objects.all()
-    context = {'posts':posts}
+    
+    #Pagination
+    post_per_page = 3
+    paginator = Paginator(posts, post_per_page)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    context = {'page_obj':page_obj}
     return render(request, 'home/index.html', context)
 
 # post_detail
